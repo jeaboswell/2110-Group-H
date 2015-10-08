524e5221e7c78e4ac57e45a040ae69f4d7c27c7f
Jesse <jeaboswell@gmail.com>
2015-10-08 14:31:06 -0500

    Added a help menu

--- Program 1/Program 1/functions.cpp
+++ Program 1/Program 1/functions.cpp
@@ -243,6 +243,13 @@ bool runCommand(string command)
 	}
 	#pragma endregion
 
+	#pragma region Help Page
+	else if (splitCommands.front() == "HELP")
+	{
+
+	}
+	#pragma endregion
+
 	#pragma region Quit
 	else if (splitCommands.front() == "QUIT")
 		quit = true;
@@ -554,5 +561,50 @@ void parseFile(string fileName)
 			cout << "\a*** ERROR *** NO FILE IN PATH: " << fileName << endl;
 		NEWLINE
 	}
+}
+#pragma endregion
+
+#pragma region Help Function
+void printHelp()
+{
+	NEWLINE
+		cout << "Formatting help:" << endl;
+	NEWLINE
+		cout << left << setw(20) << "Nodes:" << setfill('-') << endl;
+		cout << "Adding a node to the program:" << endl;
+		cout << "\tnode add <NAME>" << endl;
+		cout << "Deleting a node from the program:" << endl;
+		cout << "\tnode delete <NAME>" << endl;
+		cout << "Searching the program for an existing node:" << endl;
+		cout << "\tnode search <NAME>" << endl;
+	NEWLINE
+		cout << left << setw(20) << "Edges:" << setfill('-') << endl;
+		cout << "Adding an edge to the program:" << endl;
+		cout << "\tedge add <START NODE> <END NODE>" << endl;
+		cout << "Deleting an edge from the program:" << endl;
+		cout << "\tedge delete <START NODE> <END NODE>" << endl;
+	NEWLINE
+		cout << left << setw(17) << "Printing:" << setfill('-') << endl;
+		cout << "Printing the adjacency matrix:" << endl;
+		cout << "\tprint matrix" << endl;
+		cout << "Printing the adjacency list:" << endl;
+		cout << "\tprint list" << endl;
+	NEWLINE
+		cout << left << setw(20) << "Command File:" << setfill('-') << endl;
+		cout << "Using a .csv file to execute commands:" << endl;
+		cout << "\tfile <FILE PATH>" << endl;
+		cout << endl << "\t\tThe file path may NOT contain any spaces" << endl;
+	NEWLINE
+		cout << left << setw(20) << "Other Commands:" << setfill('-') << endl;
+		cout << "Clear the screen:" << endl;
+		cout << "\tclear" << endl;
+		cout << "List the contents of the program folder:" << endl;
+		cout << "\tls" << endl;
+		cout << "Get a list of all commands:" << endl;
+		cout << "\thelp" << endl;
+		cout << "Exit the program:" << endl;
+		cout << "\tquit" << endl;
+	NEWLINE
+	KEYPRESS
 }
 #pragma endregion
